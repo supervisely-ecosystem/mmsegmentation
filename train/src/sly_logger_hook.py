ad450af17889aedfefd2780404708de8212b9085
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2023-05-04 16:52:44 +0300

    fix iter value for chart (#32)
    * add train logs
    
    * add logs
    
    * add epoch_float log
    
    * add fix for iter
    
    * add progress_ter_value log
    
    * fix iter value
    
    * fix iter value
    
    * fix iter value
    
    * fix iter value
    
    * comment debug info

--- train/src/sly_logger_hook.py
+++ train/src/sly_logger_hook.py
@@ -9,24 +9,23 @@ import classes as cls
 
 @HOOKS.register_module()
 class SuperviselyLoggerHook(TextLoggerHook):
-    def __init__(self,
-                 by_epoch=True,
-                 interval=10,
-                 ignore_last=True,
-                 reset_flag=False,
-                 interval_exp_name=1000):
-        super(SuperviselyLoggerHook, self).__init__(by_epoch, interval, ignore_last, reset_flag, interval_exp_name)
+    def __init__(
+        self, by_epoch=True, interval=10, ignore_last=True, reset_flag=False, interval_exp_name=1000
+    ):
+        super(SuperviselyLoggerHook, self).__init__(
+            by_epoch, interval, ignore_last, reset_flag, interval_exp_name
+        )
         self.progress_epoch = None
         self.progress_iter = None
         self._lrs = []
 
     def _log_info(self, log_dict, runner):
-        if log_dict['mode'] == 'train' and 'time' in log_dict.keys():
-            self.time_sec_tot += (log_dict['time'] * self.interval)
+        if log_dict["mode"] == "train" and "time" in log_dict.keys():
+            self.time_sec_tot += log_dict["time"] * self.interval
             time_sec_avg = self.time_sec_tot / (runner.iter - self.start_iter + 1)
             eta_sec = time_sec_avg * (len(runner.data_loader) * runner.max_epochs - runner.iter - 1)
             eta_str = str(datetime.timedelta(seconds=int(eta_sec)))
-            log_dict['sly_eta'] = eta_str
+            log_dict["sly_eta"] = eta_str
 
         if self.progress_epoch is None:
             self.progress_epoch = sly.Progress("Epochs", runner.max_epochs)
@@ -34,11 +33,18 @@ class SuperviselyLoggerHook(TextLoggerHook):
             self.progress_iter = sly.Progress("Iterations", len(runner.data_loader))
 
         fields = []
-        if log_dict['mode'] == 'train':
+        if log_dict["mode"] == "train":
             self.progress_epoch.set_current_value(log_dict["epoch"])
-            self.progress_iter.set(log_dict['iter'] % len(runner.data_loader), len(runner.data_loader))
-            fields.append({"field": "data.eta", "payload": log_dict['sly_eta']})
-            is_val = log_dict['iter'] % len(runner.data_loader) == 0
+            if log_dict["iter"] % len(runner.data_loader) == 0:
+                progress_iter_value = float(self.progress_iter.total)
+                # log_dict["iter"] / len(runner.data_loader)
+            else:
+                progress_iter_value = log_dict["iter"] % len(runner.data_loader)
+
+            # sly.logger.debug(f"progress_iter_value: {progress_iter_value}")
+            self.progress_iter.set(progress_iter_value, len(runner.data_loader))
+            fields.append({"field": "data.eta", "payload": log_dict["sly_eta"]})
+            is_val = log_dict["iter"] % len(runner.data_loader) == 0
             fields.append({"field": "state.isValidation", "payload": is_val})
         else:
             fields.append({"field": "state.isValidation", "payload": True})
@@ -46,15 +52,25 @@ class SuperviselyLoggerHook(TextLoggerHook):
         add_progress_to_request(fields, "Epoch", self.progress_epoch)
         add_progress_to_request(fields, "Iter", self.progress_iter)
 
-        epoch_float = \
-            float(self.progress_epoch.current) + float(self.progress_iter.current) / float(self.progress_iter.total)
-        if log_dict['mode'] == 'train':
-            fields.extend([
-                {"field": "state.chartLR.series[0].data", "payload": [[epoch_float, round(log_dict["lr"], 6)]], "append": True},
-                {"field": "state.chartTrainLoss.series[0].data", "payload": [[epoch_float, round(log_dict["loss"], 6)]],
-                 "append": True},
-            ])
-            '''
+        epoch_float = float(self.progress_epoch.current) + float(
+            self.progress_iter.current
+        ) / float(self.progress_iter.total)
+        if log_dict["mode"] == "train":
+            fields.extend(
+                [
+                    {
+                        "field": "state.chartLR.series[0].data",
+                        "payload": [[epoch_float, round(log_dict["lr"], 6)]],
+                        "append": True,
+                    },
+                    {
+                        "field": "state.chartTrainLoss.series[0].data",
+                        "payload": [[epoch_float, round(log_dict["loss"], 6)]],
+                        "append": True,
+                    },
+                ]
+            )
+            """
             self._lrs.append(log_dict["lr"])
             fields.append({
                 "field": "state.chartLR.options.yaxisInterval",
@@ -63,21 +79,32 @@ class SuperviselyLoggerHook(TextLoggerHook):
                     round(max(self._lrs) + max(self._lrs) / 10.0, 5)
                 ]
             })
-            '''
+            """
 
-            if 'time' in log_dict.keys():
-                fields.extend([
-                    {"field": "state.chartTime.series[0].data", "payload": [[epoch_float, log_dict["time"]]],
-                     "append": True},
-                    {"field": "state.chartDataTime.series[0].data", "payload": [[epoch_float, log_dict["data_time"]]],
-                     "append": True},
-                    {"field": "state.chartMemory.series[0].data", "payload": [[epoch_float, log_dict["memory"]]],
-                     "append": True},
-                ])
-        if log_dict['mode'] == 'val':
+            if "time" in log_dict.keys():
+                fields.extend(
+                    [
+                        {
+                            "field": "state.chartTime.series[0].data",
+                            "payload": [[epoch_float, log_dict["time"]]],
+                            "append": True,
+                        },
+                        {
+                            "field": "state.chartDataTime.series[0].data",
+                            "payload": [[epoch_float, log_dict["data_time"]]],
+                            "append": True,
+                        },
+                        {
+                            "field": "state.chartMemory.series[0].data",
+                            "payload": [[epoch_float, log_dict["memory"]]],
+                            "append": True,
+                        },
+                    ]
+                )
+        if log_dict["mode"] == "val":
             class_metrics = {}
             for field_name, field_val in log_dict.items():
-                field_name_parts = field_name.split('.')
+                field_name_parts = field_name.split(".")
                 if len(field_name_parts) == 1:
                     continue
                 metric, class_name = field_name_parts[0], field_name_parts[1]
@@ -86,23 +113,35 @@ class SuperviselyLoggerHook(TextLoggerHook):
                     class_metrics[metric] = {}
                 class_metrics[metric][class_name] = [[log_dict["epoch"], field_val]]
             for metric_name, metrics in class_metrics.items():
-                if "m" + metric_name not in g.evalMetrics:
+                if f"m{metric_name}" not in g.evalMetrics:
                     continue
                 classes = cls.selected_classes + ["__bg__"]
                 for class_ind, class_name in enumerate(classes):
-                    fields.extend([
-                        {"field": f"state.class_charts.chartVal_{metric_name}.series[{class_ind}].data",
-                         "payload": metrics[class_name], "append": True}
-                    ])
+                    fields.extend(
+                        [
+                            {
+                                "field": f"state.class_charts.chartVal_{metric_name}.series[{class_ind}].data",
+                                "payload": metrics[class_name],
+                                "append": True,
+                            }
+                        ]
+                    )
             for metric_name in g.evalMetrics:
-                metric_values = [metric_val[0][1] for metric_val in list(class_metrics[metric_name[1:]].values())]
+                metric_values = [
+                    metric_val[0][1] for metric_val in list(class_metrics[metric_name[1:]].values())
+                ]
                 mean_metric = sum(metric_values) / len(metric_values)
-                fields.extend([
-                    {"field": f"state.mean_charts.chartVal_{metric_name}.series[0].data",
-                     "payload": [[log_dict["epoch"], mean_metric]], "append": True}
-                ])
+                fields.extend(
+                    [
+                        {
+                            "field": f"state.mean_charts.chartVal_{metric_name}.series[0].data",
+                            "payload": [[log_dict["epoch"], mean_metric]],
+                            "append": True,
+                        }
+                    ]
+                )
         try:
             g.api.app.set_fields(g.task_id, fields)
         except Exception as e:
-            print("Unabled to write metrics to chart!")
+            print("Unable to write metrics to chart!")
             print(e)
